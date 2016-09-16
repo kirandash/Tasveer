5eb1e09c95905658687c671a0025e718805dfc7e
dashkiran <Kiran@INFASTA.COM>
2016-09-16 17:24:14 +0530

    Create Custom Options with Settings API

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -19,8 +19,31 @@ function tasveer_add_admin_page() {
 	/*add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '' )*/
 	add_submenu_page( 'tasveer-theme-options', 'Tasveer Theme Options', 'Tasveer Theme', 'manage_options', 'tasveer-theme-options', 'tasveer_create_page' );
 	add_submenu_page( 'tasveer-theme-options', 'Tasveer Custom CSS', 'Tasveer CSS', 'manage_options', 'tasveer-theme-css', 'tasveer_css_page' );
+	
+	// Activate custom settings
+	add_action( 'admin_init', 'tasveer_custom_settings' );
 }
 add_action( 'admin_menu', 'tasveer_add_admin_page' );
+
+function tasveer_custom_settings() {
+	 // add_settings_section( $id, $title, $callback, $page );
+	 add_settings_section( 'tasveer-sidebar-options', 'Sidebar Options', 'tasveer_sidebar_options', 'tasveer-theme-options' );
+	 
+	 // add_settings_field( $id, $title, $callback, $page, $section, $args );
+	 add_settings_field( 'tasveer-sidebar-name', 'First Name', 'tasveer_sidebar_name', 'tasveer-theme-options', 'tasveer-sidebar-options' );
+	 
+	 // register_setting( $option_group, $option_name, $sanitize_callback );
+	 register_setting( 'tasveer-theme-settings-group', 'first_name' );
+}
+
+function tasveer_sidebar_options() {
+	echo 'Customize your sidebar information.';
+}
+
+function tasveer_sidebar_name() {
+	$firstName = esc_attr( get_option('first_name') );
+	echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name">';
+}
 
 function tasveer_create_page() {
 	// Generation of our admin page
--- inc/templates/tasveer-admin.php
+++ inc/templates/tasveer-admin.php
@@ -1,3 +1,11 @@
 <h1>Tasveer Theme Opions</h1>
-<h3 class="title">Manage Options</h3>
-<p>Customize the default Tasveer appearance Options</p>
+<?php settings_errors(); ?>
+<form method="post" action="options.php">
+	<?php 
+	// do_settings_sections( $page ); Prints out all settings sections added to a particular settings page.
+	do_settings_sections( 'tasveer-theme-options' ); 
+	// settings_fields( $option_group );
+	settings_fields( 'tasveer-theme-settings-group' );
+	submit_button();
+	?>
+</form>
