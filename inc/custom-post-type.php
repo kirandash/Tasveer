803788e93ff7ed4b1c895a83a51ca159d51a1206
kirandash <thekirankumardash@gmail.com>
2016-09-20 10:13:20 +0530

    11 - Manage and Create Custom Columns

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -13,6 +13,8 @@ $activateContact = get_option('activate_contact');
 
 if( $activateContact == 1 ) {
 	add_action( 'init', 'tasveer_contact_custom_post_type' );	
+	add_filter( 'manage_tasveer-contact_posts_columns', 'tasveer_set_custom_columns' );
+	add_action( 'manage_tasveer-contact_posts_custom_column', 'tasveer_custom_contact_column', 10, 2 );
 }
 
 function tasveer_contact_custom_post_type() {
@@ -35,6 +37,26 @@ function tasveer_contact_custom_post_type() {
 	);
 	
 	register_post_type( 'tasveer-contact', $args );
+}
+
+function tasveer_set_custom_columns( $columns ) {
+	$newColumn = array();
+	$newColumn['title'] 	= 'Full Name';
+	$newColumn['message'] 	= 'Message';
+	$newColumn['email']		= 'Email';
+	$newColumn['date'] 		= 'Date';
+	return $newColumn;
+}
+
+function tasveer_custom_contact_column( $column, $post_id ) {
+	switch( $column ){
+		case 'message':
+			echo get_the_excerpt();
+			break;
+		case 'email':
+			echo 'The email here';
+			break;
+	}
 }
 
 ?>
