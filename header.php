ee3c0d2ff6ac2ea07c4bafea9d5205683421a297
kirandash <thekirankumardash@gmail.com>
2016-09-21 12:29:06 +0530

    14 - Create basic File Structure - Header, Footer and enqueue

--- header.php
+++ header.php
@@ -1,1 +1,12 @@
-<?php wp_head(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+    	<meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <?php if( is_singular() && pings_open() ): ?>
+	        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+        <?php endif; ?>
+		<?php wp_head(); ?>
+    </head>
+    <body class="<?php body_class(); ?>">
