2dab40e0a6aca3a6cd0c9c8011b161142e9d4876
kirandash <thekirankumardash@gmail.com>
2016-10-01 15:05:15 +0530

    15 - Create the Header Section

--- header.php
+++ header.php
@@ -10,3 +10,21 @@
 		<?php wp_head(); ?>
     </head>
     <body class="<?php body_class(); ?>">
+    
+    	<div class="row">
+			<div class="col-xs-12">
+            	<div class="header-container background-image text-center" style="background-image: url(<?php header_image(); ?>);">
+                	<div class="header-content table">
+                    	<div class="table-cell">
+                        	<h1 class="site-title tasveer-icon">
+                            	<span class="tasveer-camera"></span>
+								<span class="hide"><?php bloginfo('name'); ?></span>
+                            </h1>
+                            <h2 class="site-description">
+                            	<?php bloginfo('description'); ?>
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- .row -->
